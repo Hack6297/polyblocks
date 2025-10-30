e11719649c15586201a2e9bc8b0393f2a1000ca0
Hack6297 <164305482+Hack6297@users.noreply.github.com>
2025-10-30 16:56:47 -0700

    Delete parkour_multiplayer_backend directory

--- parkour_multiplayer_backend/multiplayer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('join/', views.join_game),
-    path('players/', views.player_positions),
-    path('rooms/', views.room_list_create),
-]
